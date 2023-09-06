deecaa1458e99856fb768f45cf9f1acece123b22
Dear.Va <1328886154@qq.com>
2023-09-07 01:05:34 +0800

    修改命名

--- Antelcat.Extensions/TimeExtension.cs
+++ Antelcat.Extensions/TimeExtension.cs
@@ -5,10 +5,14 @@ namespace Antelcat.Extensions;
 public static class TimeExtension
 {
     public static DateTime Timebase { get; } = new (1970, 1, 1);
+    
     public static DateTime UtcTimebase { get; } = new (1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-    public static long CurrentTimestamp => DateTimeOffset.Now.ToUnixTimeMilliseconds();
+    public static long CurrentUnixTimestamp => DateTimeOffset.Now.ToUnixTimeMilliseconds();
     public static DateTime ToLocalTime(this long timestamp) => DateTimeOffset.FromUnixTimeMilliseconds(timestamp).LocalDateTime;
+    
     public static DateTime ToUtcTime(this long timestamp) => DateTimeOffset.FromUnixTimeMilliseconds(timestamp).UtcDateTime;
-    public static long ToTimestamp(this DateTime dateTime) => new DateTimeOffset(dateTime).ToUnixTimeMilliseconds();
+    
+    public static long ToUnixTimestamp(this DateTime dateTime) => new DateTimeOffset(dateTime).ToUnixTimeMilliseconds();
+    
     public static TaskAwaiter GetAwaiter(this int millionSeconds) => Task.Delay(millionSeconds).GetAwaiter();
 }
